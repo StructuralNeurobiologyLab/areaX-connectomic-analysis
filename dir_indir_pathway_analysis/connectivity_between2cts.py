5f28a02a1629522f2543f8f8d1b66e9a8a65b0af
arother <rother@neuro.mpg.de>
2021-08-18 15:50:30 +0200

    add function to compare volume estimation of compartment between two celltypes to compartment_volume_celltype. Started writing notes about function connectivity_between2cts that is supposed to give information about the synapse size, amount and distribution between two celltypes

---
+++ dir_indir_pathway_analysis/connectivity_between2cts.py
@@ -0,0 +1,4 @@
+#write analysis to compare connectivity of two celltypes
+# compare amount of synapses, average synapse area, average amount of synapses from one cell to another, average size of synapses from one cell to another
+#also compare percentage onto spine vs shaft from axo_dendritic synapses
+#amount and average size of soma synapses, percentage of soma synapses from totl

abeb912f3acee0ddaec0b2ee8d53f344e8d43e5d
arother <rother@neuro.mpg.de>
2022-04-01 12:51:34 +0200

    continued writing subpopulations_per_connectivity

--- dir_indir_pathway_analysis/subpopulations_per_connectivity.py
+++ dir_indir_pathway_analysis/subpopulations_per_connectivity.py
@@ -44,20 +44,90 @@ def sort_by_connectivity(sd_synssv, ct1, ct2, ct3, cellids1, cellids2, cellids3,
     time_stamps = [time.time()]
     step_idents = ['t-0']
 
-    log.info("Step 1/X Check compartment length of cells from three celltypes")
+    log.info("Step 1/X: Check compartment length of cells from three celltypes")
     cellids1 = check_comp_lengths_ct(cellids1, fullcelldict=full_celldict1, min_comp_len=min_comp_len)
     cellids2 = check_comp_lengths_ct(cellids2, fullcelldict=full_celldict2, min_comp_len=min_comp_len)
     cellids3 = check_comp_lengths_ct(cellids3, fullcelldict=full_celldict3, min_comp_len=min_comp_len)
 
-    log.info("Step 2/X Prefilter synapses for synapses between these celltypes")
+    log.info("Step 2/X: Prefilter synapses for synapses between these celltypes")
     m_cts, m_ids, m_axs, m_ssv_partners, m_sizes, m_spiness = filter_synapse_caches_for_ct(sd_synssv,
                                                                                            pre_cts=[ct1], post_cts = [ct2, ct3],
                                                                                            syn_prob_thresh=syn_prob_thresh,
                                                                                            min_syn_size=min_syn_size,
                                                                                            axo_den_so=True)
 
-#take cached synapse arrays and sort them into four groups
-# save cellids per group as array
-# save table and dictionary which GPs they connect to, their amount and sum of synapses
-# also save compartment
+    time_stamps = [time.time()]
+    step_idents = ['t-0']
 
+    log.info("Step 3/X: Sort celltype into groups based on connectivity")
+    #filter synapses that are not from cellids 1
+    ct1ids_inds = np.any(np.in1d(m_ssv_partners, cellids1).reshape(len(m_cts), 2), axis=1)
+    m_cts = m_cts[ct1ids_inds]
+    m_ids = m_ids[ct1ids_inds]
+    m_axs = m_axs[ct1ids_inds]
+    m_ssv_partners = m_ssv_partners[ct1ids_inds]
+    m_sizes = m_sizes[ct1ids_inds]
+    m_spiness = m_spiness[ct1ids_inds]
+    #filter synapses that are not from cellids 2 or 3
+    ct2ids_inds = np.any(np.in1d(m_ssv_partners, np.hstack([cellids2, cellids3])).reshape(len(m_ssv_partners), 2), axis=1)
+    m_cts = m_cts[ct2ids_inds]
+    m_ids = m_ids[ct2ids_inds]
+    m_axs = m_axs[ct2ids_inds]
+    m_ssv_partners = m_ssv_partners[ct2ids_inds]
+    m_sizes = m_sizes[ct2ids_inds]
+    m_spiness = m_spiness[ct2ids_inds]
+    #sort according to connectivity
+    only_2ct2_cellids = np.zeros(len(cellids1))
+    only_2_ct3_cellids = np.zeros(len(cellids1))
+    #seperate synapses into connecteted to cellids2 and cellids3
+    conn_ct2_inds = np.any(np.in1d(m_ssv_partners, cellids2).reshape(len(m_ssv_partners), 2), axis=1)
+    conn_ct3_inds = np.any(np.in1d(m_ssv_partners, cellids3).reshape(len(m_ssv_partners), 2), axis=1)
+    ct2_ssv_partners = m_ssv_partners[conn_ct2_inds]
+    ct2_sizes = m_sizes[conn_ct2_inds]
+    ct2_spiness = m_spiness[conn_ct2_inds]
+    ct2_cts = m_cts[conn_ct2_inds]
+    ct3_ssv_partners = m_ssv_partners[conn_ct3_inds]
+    ct3_sizes = m_sizes[conn_ct3_inds]
+    ct3_spiness = m_spiness[conn_ct3_inds]
+    ct3_cts = m_cts[conn_ct3_inds]
+    #get unique ids for cellids1 from each of them
+    ct1_ct2_inds = np.where(ct2_cts == ct1)
+    ct1_ct2_ssvsids = ct2_ssv_partners[ct1_ct2_inds]
+    ct1ct2_ssv_inds, unique_ct1_ct2ssvs = pd.factorize(ct1_ct2_ssvsids)
+    ct1ct2_syn_sumsizes = np.bincount(ct1ct2_ssv_inds, ct2_sizes)
+    ct1ct2_syn_amounts = np.bincount(ct1ct2_ssv_inds)
+    ct1_ct3_inds = np.where(ct3_cts == ct1)
+    ct1_ct3_ssvsids = ct3_ssv_partners[ct1_ct3_inds]
+    ct1ct3_ssv_inds, unique_ct1_ct3ssvs = pd.factorize(ct1_ct3_ssvsids)
+    ct1ct3_syn_sumsizes = np.bincount(ct1ct3_ssv_inds, ct3_sizes)
+    ct1ct3_syn_amounts = np.bincount(ct1ct3_ssv_inds)
+    #split in three groups: oonly ct2, only ct3, both
+    both_inds = np.in1d(unique_ct1_ct2ssvs, unique_ct1_ct3ssvs)
+    both_inds2 = np.in1d(unique_ct1_ct3ssvs, unique_ct1_ct2ssvs)
+    both_cellids = unique_ct1_ct2ssvs[both_inds]
+    both_syn_sumsizes = ct1ct2_syn_sumsizes[both_inds] + ct1ct3_syn_sumsizes[both_inds2]
+    both_syn_amounts = ct1ct2_syn_amounts[both_inds] + ct1ct3_syn_amounts[both_inds2]
+    only_2ct2_cellids = unique_ct1_ct2ssvs[both_inds == False]
+    only_ct1ct2_syn_sumsizes = ct1ct2_syn_sumsizes[both_inds == False]
+    only_ct1ct2_syn_amounts = ct1ct2_syn_amounts[both_inds == False]
+    only_2ct3_cellids = unique_ct1_ct3ssvs[both_inds2 == False]
+    only_ct1ct3_syn_sumsizes = ct1ct3_syn_sumsizes[both_inds2 == False]
+    only_ct1ct3_syn_amounts = ct1ct3_syn_amounts[both_inds2 == False]
+    # make dictionary for each group
+    only_ct2_dict = {cellid: {"synapse amount": only_ct1ct2_syn_amounts[i], "sum size synapses": only_ct1ct2_syn_sumsizes[i]} for i, cellid in enumerate(only_2ct2_cellids)}
+    only_ct3_dict = {
+        cellid: {"synapse amount": only_ct1ct3_syn_amounts[i], "sum size synapses": only_ct1ct3_syn_sumsizes[i]} for
+        i, cellid in enumerate(only_2ct3_cellids)}
+    both_dict = {cellid: {"synapse amount": both_syn_amounts[i], "sum size synapses": both_syn_sumsizes[i]} for i, cellid in enumerate(both_cellids)}
+    #those that are in fullcells of ct1 and do not have nay connectivity here are in group four
+    connected_cellids1 = np.hstack([only_2ct3_cellids, only_2ct2_cellids, both_cellids])
+    not_conn_inds = np.in1d(cellids1, connected_cellids1) == False
+    not_connected_ids = cellids1[not_conn_inds]
+
+    #save GPe, GPiids, they are connected to, compartments they are connected to
+    #save data in dataframe and table
+    #plot results and compute statistical values between groups
+    #also include percentage of overall synamount for outgoing synapses
+    #maybe even incoming synapses as GPe/i
+    #save dictionaries and arrays for further use
+    #then rewrite other functions to compare up to 4 groups

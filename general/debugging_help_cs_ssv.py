812fd3c0bba1d307d1092d33a14f7f52fa63cc76
arother <rother@neuro.mpg.de>
2022-06-27 17:53:47 +0200

    remove bugs

--- general/debugging_help_cs_ssv.py
+++ general/debugging_help_cs_ssv.py
@@ -5,6 +5,7 @@ from syconn import global_params
 from syconn.reps.segmentation import SegmentationDataset
 from syconn.extraction import cs_processing_steps as cps
 from syconn.reps.super_segmentation import SuperSegmentationDataset, SuperSegmentationObject
+from time import time
 
 
 global_params.wd = "/ssdscratch/songbird/j0251/j0251_72_seg_20210127_agglo2"
@@ -39,7 +40,9 @@ cs_ssv_ids = sd_cs_ssv.ids
 cs_ssv_coords = sd_cs_ssv.load_numpy_data("rep_coord")
 cs_ssv_cs_ids = sd_cs_ssv.load_numpy_data("cs_ids")
 cs_ssv_partners = sd_cs_ssv.load_numpy_data("neuron_partners")
-cs_ssv_sizes = sd_cs_ssv.load_numpy_data["size"]
+cs_ssv_sizes = sd_cs_ssv.load_numpy_data("size")
+
+start = time()
 
 #get parameters for exmaple syn id
 synid1_index = np.where(syn_ids == synid1)[0]
@@ -63,18 +66,38 @@ c12_cs_ssv_partners = c1_cs_ssv_partners[c12_mask]
 c12_cs_ssv_cs_ids = c1_cs_ssv_cs_ids[c12_mask]
 c12_cs_ssv_coords = c1_cs_ssv_coords[c12_mask]
 c12_cs_ssv_sizes = c1_cs_ssv_sizes[c12_mask]
+print(f"cs_ssv coords: {c12_cs_ssv_coords}")
+
+#get synapses between cellids
+sync1_mask = np.any(syn_partners == cellid1, axis = 1)
+c1_syn_ids = syn_ids[sync1_mask]
+c1_syn_partners = syn_partners[sync1_mask]
+c1_syn_cs_ids = syn_cs_ids[sync1_mask]
+c1_syn_coords = syn_coords[sync1_mask]
+c1_syn_sizes = syn_sizes[sync1_mask]
+sync12_mask = np.any(c1_syn_partners == cellid2, axis = 1)
+c12_syn_ids = c1_syn_ids[sync12_mask]
+c12_syn_partners = c1_syn_partners[sync12_mask]
+c12_syn_cs_ids = c1_syn_cs_ids[sync12_mask]
+c12_syn_coords = c1_syn_coords[sync12_mask]
+c12_syn_sizes = c1_syn_sizes[sync12_mask]
+print(f"syn_ssv coords: {c12_syn_coords}")
+
+print(f"{time() - start}")
+
+#raise ValueError
 
 #get corresponding cs_parameters
-cs_syn_index = np.where(cs_ids == synid1_cs_id)[0]
-cs_syn_coord = cs_coords[cs_syn_index]
-cs_syn_size = cs_sizes[cs_syn_index]
+#cs_syn_index = np.where(cs_ids == synid1_cs_id)[0][0]
+#cs_syn_coord = cs_coords[cs_syn_index]
+#cs_syn_size = cs_sizes[cs_syn_index]
 
-cs_cs1_index = np.where(cs_ids == c12_cs_ssv_cs_ids[0])[0]
-cs_cs1_coord = cs_coords[cs_cs1_index]
-cs_cs1_size = cs_sizes[cs_cs1_index]
+#cs_cs1_index = np.where(cs_ids == c12_cs_ssv_cs_ids[0])[0]
+#cs_cs1_coord = cs_coords[cs_cs1_index]
+#cs_cs1_size = cs_sizes[cs_cs1_index]
 
 #go through steps in cps
-filtered_cs = cps.filter_relevant_syn(sd_cs, ssd, log = None)
+#filtered_cs = cps.filter_relevant_syn(sd_cs, ssd, log = None)
 
 
 

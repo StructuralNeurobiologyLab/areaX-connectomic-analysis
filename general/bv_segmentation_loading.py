0471a6bff2135fd2572303f8898a71dcadf74c8f
arother <rother@neuro.mpg.de>
2023-06-28 15:43:43 +0200

    add option to have more than just top 100 components

--- general/bv_segmentation_loading.py
+++ general/bv_segmentation_loading.py
@@ -15,15 +15,16 @@ def find_border_voxels_padded(data):
 import h5py
 import numpy as np
 from scipy import ndimage
-import time
 from scipy.ndimage import zoom
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from numba import njit
 import os as os
 from syconn.handler.config import initialize_logging
-from mayavi import mlab
+#from mayavi import mlab
 import open3d as o3d
+import trimesh
+from skimage import measure
 
 
 f_name = 'cajal/scratch/users/arother/bio_analysis_results/general/230723_bv_seg_rendering'
@@ -33,7 +34,7 @@ if not os.path.exists(f_name):
     os.mkdir(f_name)
 
 hdf5_path = "/cajal/nvmescratch/projects/songbird/j0251/j0251_raw_near_iso_16x_ds_rfc_bloodvessel_seg.h5"
-log.info(f'Step 1/X: Read data from hdf5 with path {hdf5_path}')
+log.info(f'Step 1/5: Read data from hdf5 with path {hdf5_path}')
 # Read the hdf5 file with a 3D matrix
 with h5py.File( hdf5_path,"r") as f:
     data = f["dataset_1"][:]
@@ -41,21 +42,22 @@ data = zoom(data, (0.5,0.5,0.5), order=0)
 mask = data == 2
 
 dil_iterations = 2
-log.info(f'Step 2/X: Perform closing operation with iterations = {dil_iterations}')
+log.info(f'Step 2/5: Perform closing operation with iterations = {dil_iterations}')
 #make figure pre closing
 plt.title('pre closing')
-plt.plot(mask[100,:,:])
+plt.imshow(mask[100,:,:])
 plt.savefig(f'{f_name}/pre_closing.png')
 plt.close()
 mask = ndimage.binary_dilation(mask, iterations = dil_iterations)
 mask = ndimage.binary_closing(mask)
 plt.title('post closing')
-plt.plot(mask[100,:,:])
+plt.imshow(mask[100,:,:])
 plt.savefig(f'{f_name}/post_closing.png')
 plt.close()
 
-log.info('Step 3/X: Get voxel number and boundary voxels from surface filtering')
+log.info('Step 3/5: Get voxel number and boundary voxels from surface filtering')
 labeled_mask, num_labels = ndimage.label(mask)
+bv_seg = mask.astype(float)
 log.info(f'num_labels: {num_labels}')
 # Calculate the number of voxels in each connected component
 component_voxel_counts = np.bincount(labeled_mask.ravel())
@@ -64,15 +66,27 @@ component_voxel_counts = np.bincount(labeled_mask.ravel())
 component_voxel_counts[0] = 0
 
 # Find the indices of the top 1000-largest connected components
-top_indices = np.argsort(component_voxel_counts, )[-100:]
+top_indices100 = np.argsort(component_voxel_counts, )[-100:]
+top_indices1000 = np.argsort(component_voxel_counts, )[-1000:]
 # Create a mask for the top 1000-largest connected components
-top_indices = np.isin(labeled_mask, top_indices, invert=False)
-log.info(f'num bv voxels: {np.sum(top_indices>0)}')
-log.info(f'num background voxels: {np.sum(top_indices==0)}')
+top_indices100 = np.isin(labeled_mask, top_indices100, invert=False)
+top_indices1000 = np.isin(labeled_mask, top_indices1000, invert=False)
+log.info(f'num bv voxels in top 100 components: {np.sum(top_indices100>0)}')
+log.info(f'num background voxels top 100 components: {np.sum(top_indices100==0)}')
+log.info(f'num bv voxels in top 1000 components: {np.sum(top_indices1000>0)}')
+log.info(f'num background voxels top 1000 components: {np.sum(top_indices1000==0)}')
 
 plt.title('pre surface filtering')
-plt.plot(top_indices[100,:,:])
-plt.savefig(f'{f_name}/pre_surface_filtering.png')
+plt.imshow(top_indices100[100,:,:])
+plt.savefig(f'{f_name}/pre_surface_filtering_top100.png')
+plt.close()
+plt.title('pre surface filtering')
+plt.imshow(top_indices1000[100,:,:])
+plt.savefig(f'{f_name}/pre_surface_filtering_top1000.png')
+plt.close()
+plt.title('pre surface filtering')
+plt.imshow(bv_seg[100,:,:])
+plt.savefig(f'{f_name}/pre_surface_filtering_alldata.png')
 plt.close()
 
 # only boundary voxels
@@ -110,25 +124,40 @@ def find_border_voxels(data):
 
     return border_voxels
 
-top_indices_surface = find_border_voxels_padded(top_indices)
-log.info(f'num bv voxels surface only: {np.sum(top_indices_surface>0)}')
+top_indices100_surface = find_border_voxels_padded(top_indices100)
+top_indices1000_surface = find_border_voxels_padded(top_indices1000)
+bv_seg_surface = find_border_voxels_padded(bv_seg)
+log.info(f'num bv voxels surface only top 100: {np.sum(top_indices100_surface>0)}')
+log.info(f'num bv voxels surface only top 1000: {np.sum(top_indices1000_surface>0)}')
+log.info(f'num bv voxels surface only whole bv seg: {np.sum(bv_seg_surface>0)}')
 
 plt.title('post surface filtering')
-plt.plot(top_indices_surface[100,:,:])
-plt.savefig(f'{f_name}/post_surface_filtering.png')
+plt.imshow(top_indices100_surface[100,:,:])
+plt.savefig(f'{f_name}/post_surface_filtering_top100.png')
+plt.close()
+plt.title('post surface filtering')
+plt.imshow(top_indices1000_surface[100,:,:])
+plt.savefig(f'{f_name}/post_surface_filtering_top1000.png')
+plt.close()
+plt.title('post surface filtering')
+plt.imshow(bv_seg_surface[100,:,:])
+plt.savefig(f'{f_name}/post_surface_filtering_bv_seg.png')
 plt.close()
 
-log.info('Step 4/X: create figure of results, also volume')
+log.info('Step 4/5: create figure of results, also volume')
+
 # Update the coordinates of the voxels with values above the threshold
-z, y, x = np.where(top_indices_surface)
+#z, y, x = np.where(top_indices_surface)
 
 # Create a histogram of the number of voxels in all the components
+'''
 plt.hist(component_voxel_counts, bins='auto')
 plt.title("Histogram of voxel counts in connected components")
 plt.xlabel("Number of voxels")
 plt.ylabel("Frequency")
 plt.savefig(f'{f_name}/hist_count_components.png')
 plt.close()
+'''
 
 # Create a figure
 fig = plt.figure()
@@ -148,7 +177,7 @@ ax.set_zlabel('Z')
 # Show the plot
 plt.savefig(f'{f_name}/bv_voxel_coords.png')
 plt.close()
-
+'''
 # Create a figure
 mlab.figure(bgcolor=(1, 1, 1))
 # Prepare the volume data. Mayavi expects data in the form of a 3D array.
@@ -180,9 +209,10 @@ plt.close()
 #for idx in ten_largest_indices:
 #    print(f"Component {idx}: {component_voxel_counts[idx]} voxels")
 #for filtering
+'''
 log.info('Step 5/5: Create mesh from surfaces')
 # Get the coordinates of the voxels with values above the threshold
-z, y, x = np.where(top_indices_surface)
+z, y, x = np.where(top_indices100_surface)
 # Combine the coordinates into a single array
 points = np.column_stack((x, y, z))
 #tranfer points to nm
@@ -190,19 +220,23 @@ points_nm = points * [320, 320, 400]
 #tranfer to voxel data
 points_vx = points_nm / [10, 10, 25]
 #the following code on how to generate a mesh is not based on JK code but on a CHhatGPT suggestions from  AR
-# Create an Open3D point cloud
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(points_vx)
+top_seg100 = top_indices100_surface.astype(float)
+top_seg1000 = top_indices1000_surface.astype(float)
+full_seg = bv_seg_surface.astype(float)
+#from JH brain segmentation,
+verts100, faces100, normals100, values100 = measure.marching_cubes(top_seg100, 0)
+mesh100 = trimesh.Trimesh(verts100, faces100, validate=True)
+output_file = f'{f_name}/bv_top100_surface_mesh.ply'
+mesh100.export(output_file)
+verts1000, faces1000, normals1000, values1000 = measure.marching_cubes(top_seg1000, 0)
+mesh1000 = trimesh.Trimesh(verts1000, faces1000, validate=True)
+output_file = f'{f_name}/bv_top1000_surface_mesh.ply'
+mesh1000.export(output_file)
+verts_bv, faces_bv, normals_bv, values_bv = measure.marching_cubes(full_seg, 0)
+mesh_bv = trimesh.Trimesh(verts_bv, faces_bv, validate=True)
+output_file = f'{f_name}/bv_full_seg_surface_mesh.ply'
+mesh_bv.export(output_file)
 
-# Estimate normals for the point cloud
-pcd.estimate_normals()
-
-# Perform Poisson surface reconstruction
-mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd)
-
-# Export mesh as .ply file
-output_file = f'{f_name}/bv_top_surface_mesh.ply'
-o3d.io.write_triangle_mesh(output_file, mesh)
 log.info('Mesh of bloodvessels created and exported')
 
 '''

10a6ce31356bd1bfe07f5281ddc6e69da8873709
arother <rother@neuro.mpg.de>
2023-11-08 14:24:05 +0100

    remove bugs

--- for_eval/proj_axon_length_comparison.py
+++ for_eval/proj_axon_length_comparison.py
@@ -50,10 +50,31 @@ if __name__ == '__main__':
     log.info('Step 2/4: Get total length from all cellids')
     all_lengths = start_multiprocess_imap(get_cell_length, all_ax_ids)
     axon_df['skeleton length'] = np.array(all_lengths)
+    #remove fragments with length = 0
+    zero_lengths_ind = np.where(np.array(all_lengths) == 0)[0]
+    zero_lengths_df = axon_df.loc[zero_lengths_ind]
+    zero_lengths_df.to_csv(f'{f_name}/zero_lengths.csv')
+    nonzero_axon_df = axon_df.replace(0, np.nan)
+    nonzero_axon_df = nonzero_axon_df.dropna()
+    nonzero_axon_df = nonzero_axon_df.reset_index()
+    assert(len(axon_df) == len(zero_lengths_df) + len(nonzero_axon_df))
+    da_0 = len(zero_lengths_df[zero_lengths_df['celltype'] == 'DA'])
+    hvc_0 = len(zero_lengths_df[zero_lengths_df['celltype'] == 'HVC'])
+    lman_0 = len(zero_lengths_df[zero_lengths_df['celltype'] == 'LMAN'])
+    log.info(f'{len(zero_lengths_df)} ids were removed because length was 0; {da_0} DA, {hvc_0} HVC, {lman_0} LMAN')
+    axon_df = nonzero_axon_df
     axon_df.to_csv(f'{f_name}/proj_axon_lengths.csv')
+    #get parameters for cell ids
+    ax_groups_str = np.unique(axon_df['celltype'])
+    param_df = pd.DataFrame(columns = ax_groups_str, index = ['total number', 'mean length', 'median length'])
     for ct in ax_cts:
         ct_lengths = axon_df[axon_df['celltype'] == ct_dict[ct]]
-        ct_lengths.to_csv(f'{f_name}/pro')
+        ct_lengths.to_csv(f'{f_name}/lengths_{ct_dict[ct]}.csv')
+        param_df.loc['total number', ct_dict[ct]] = len(ct_lengths)
+        param_df.loc['mean length', ct_dict[ct]] = np.mean(ct_lengths)
+        param_df.loc['median length', ct_dict[ct]] = np.median(ct_lengths)
+
+    param_df.to_csv(f'{f_name}/summary_params.csv')
 
     log.info('Step 3/4: Calculate statistics')
     lengths_groups = [group['skeleton length'].values for name, group in
@@ -61,7 +82,6 @@ if __name__ == '__main__':
     kruskal_res = kruskal(*lengths_groups, nan_policy='omit')
     log.info(f'Kruskal results: stats = {kruskal_res[0]}, p-value = {kruskal_res[1]}')
     if kruskal_res[1] < 0.05:
-        ax_groups_str = np.unique(axon_df['celltype'])
         group_comps = list(combinations(range(len(ax_groups_str)), 2))
         ranksum_columns = [f'{ax_groups_str[gc[0]]} vs {ax_groups_str[gc[1]]}' for gc in group_comps]
         ranksum_res_df = pd.DataFrame(columns=ranksum_columns, index = ['stats', 'p-value'])
@@ -97,7 +117,6 @@ if __name__ == '__main__':
     plt.close()
     sns.histplot(data=axon_df, x='skeleton length', hue='celltype', fill=False,
                  kde=False, element='step', log_scale=True, stat='percent')
-    plt.ylabel('number of axons')
     plt.xlabel('skeleton length [µm]')
     plt.title('Lengths of axon fragments')
     plt.savefig(f'{f_name}/proj_axons_lengths_log_perc.png')

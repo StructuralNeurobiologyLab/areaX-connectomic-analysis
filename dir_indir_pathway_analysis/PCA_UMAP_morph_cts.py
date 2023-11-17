165f48e1c80e368d503b380f014b036e8be64d0a
arother <rother@neuro.mpg.de>
2023-11-17 20:15:04 +0100

    wrote down notes for two new functions; one two compare connectivity of two celltypes or more as boxplots with Hue; another one two compare morphology as PCA or uMAP

---
+++ dir_indir_pathway_analysis/PCA_UMAP_morph_cts.py
@@ -0,0 +1,10 @@
+#write function to plot morphological parameters for full cells (for two celltypes or more)
+
+#load dataframe with cellids, morphological parameters that are saved
+
+#plot them in grid plot with histplot in middle and scatterplots (see JK)
+
+#do PCA on them
+
+
+#do UMAP on them
---
+++ dir_indir_pathway_analysis/compare_conn_perc.py
@@ -0,0 +1,11 @@
+#write script here to compare connectvity between two celltypes
+#use synapse_dict_per_ct from connectivity_subgroups_per_ct
+#make boxplot with hue
+
+#Step 1: load dictionary from pkl
+
+#Step 2: get information from two (or more) celltypes to compare and put in Dataframe
+
+#Step 3: run kruskal wallis test and ranksums (for in and outgoing)
+
+#Step 4: make boxplot with hue -> hue are celltypes to compare
